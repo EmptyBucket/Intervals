9b93b01781df7304ca0d13610e4c36efd5218ff2
alexey <zxfess@yandex.ru>
2021-10-08 15:11:04 +0500

    granular interval fix constructor access

--- src/Intervals/GranularIntervals/DayInterval.cs
+++ src/Intervals/GranularIntervals/DayInterval.cs
@@ -31,7 +31,7 @@ namespace Intervals.GranularIntervals
 		public DayInterval(DateTime dateTime) : base(dateTime, dateTime.AddDays(1), IntervalType.RightOpen) =>
 			DateTime = dateTime;
 
-		public DayInterval(IPoint<DateTime> left, IPoint<DateTime> right) : base(left, right) => DateTime = left.Value;
+		private DayInterval(IPoint<DateTime> left, IPoint<DateTime> right) : base(left, right) => DateTime = left.Value;
 
 		public DateTime DateTime { get; }
 

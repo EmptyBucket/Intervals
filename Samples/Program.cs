16a48216fb8da2994ff720ebbbf14e5d57d2f102
apolitov <zxfess@yandex.ru>
2022-05-29 00:43:23 +0400

    tmp

---
+++ Samples/Program.cs
@@ -0,0 +1,31 @@
+﻿using Intervals.GranularIntervals;using Intervals.Intervals;
+
+var enumerable =
+    new[]
+        {
+            new Interval<int>(0, 10, IntervalInclusion.LeftOpened),
+            new Interval<int>(20, 30, IntervalInclusion.Closed),
+            new Interval<int>(40, 50, IntervalInclusion.RightOpened)
+        }
+        .Combine(new[]
+            {
+                new Interval<int>(10, 20, IntervalInclusion.Opened),
+                new Interval<int>(30, 40, IntervalInclusion.Opened)
+            }
+            .Subtract(new[]
+            {
+                new Interval<int>(15, 25, IntervalInclusion.Closed),
+                new Interval<int>(25, 35, IntervalInclusion.Closed)
+            }))
+        .Overlap(new[]
+            {
+                new Interval<int>(15, 20, IntervalInclusion.Closed),
+                new Interval<int>(30, 35, IntervalInclusion.Closed)
+            }
+            .SymmetricDifference(new Interval<int>(20, 35, IntervalInclusion.Opened)))
+        .Overlap(new Interval<int>(20, 25, IntervalInclusion.Opened))
+        .ToArray();
+
+var nextQuarter = new QuarterInterval(2022, 3).GetNext();
+
+Console.WriteLine();
--- src/Intervals/Intervals/Enumerable/SymmetricDifferenceEnumerable.cs
+++ src/Intervals/Intervals/Enumerable/SymmetricDifferenceEnumerable.cs
@@ -1,9 +1,23 @@
+using System.Collections.Immutable;
+
 namespace Intervals.Intervals.Enumerable;
 
 internal class SymmetricDifferenceEnumerable<T> : MergeEnumerable<T> where T : IEquatable<T>, IComparable<T>
 {
-    public SymmetricDifferenceEnumerable(IEnumerable<IInterval<T>> left, IEnumerable<IInterval<T>> right)
-        : base(left, right)
+    public static IEnumerable<IInterval<T>> Create(IEnumerable<IInterval<T>> left, IEnumerable<IInterval<T>> right)
+    {
+        var builder = ImmutableList.CreateBuilder<IEnumerable<IInterval<T>>>();
+
+        if (left is SymmetricDifferenceEnumerable<T> l) builder.AddRange(l.Batches);
+        else builder.Add(left);
+
+        if (right is SymmetricDifferenceEnumerable<T> r) builder.AddRange(r.Batches);
+        else builder.Add(right);
+
+        return new SymmetricDifferenceEnumerable<T>(builder.ToImmutable());
+    }
+
+    private SymmetricDifferenceEnumerable(IImmutableList<IEnumerable<IInterval<T>>> batches) : base(batches)
     {
     }
 

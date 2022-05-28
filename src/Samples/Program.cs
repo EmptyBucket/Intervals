d68f67dd4118b86794df87020e6c891b4ab2a34e
apolitov <zxfess@yandex.ru>
2022-05-29 01:10:31 +0400

    tmp

--- src/Samples/Program.cs
+++ src/Samples/Program.cs
@@ -21,34 +21,34 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-﻿using Intervals.GranularIntervals;using Intervals.Intervals;
+﻿using Intervals.GranularIntervals;
+using Intervals.Intervals;
 
-var enumerable =
-    new[]
+var enumerable = new[]
+    {
+        new Interval<int>(0, 10, IntervalInclusion.LeftOpened),
+        new Interval<int>(20, 30, IntervalInclusion.Closed),
+        new Interval<int>(40, 50, IntervalInclusion.RightOpened)
+    }
+    .Combine(new[]
         {
-            new Interval<int>(0, 10, IntervalInclusion.LeftOpened),
-            new Interval<int>(20, 30, IntervalInclusion.Closed),
-            new Interval<int>(40, 50, IntervalInclusion.RightOpened)
+            new Interval<int>(10, 20, IntervalInclusion.Opened),
+            new Interval<int>(30, 40, IntervalInclusion.Opened)
         }
-        .Combine(new[]
-            {
-                new Interval<int>(10, 20, IntervalInclusion.Opened),
-                new Interval<int>(30, 40, IntervalInclusion.Opened)
-            }
-            .Subtract(new[]
-            {
-                new Interval<int>(15, 25, IntervalInclusion.Closed),
-                new Interval<int>(25, 35, IntervalInclusion.Closed)
-            }))
-        .Overlap(new[]
-            {
-                new Interval<int>(15, 20, IntervalInclusion.Closed),
-                new Interval<int>(30, 35, IntervalInclusion.Closed)
-            }
-            .SymmetricDifference(new Interval<int>(20, 35, IntervalInclusion.Opened)))
-        .Overlap(new Interval<int>(20, 25, IntervalInclusion.Opened))
-        .ToArray();
+        .Subtract(new[]
+        {
+            new Interval<int>(15, 25, IntervalInclusion.Closed),
+            new Interval<int>(25, 35, IntervalInclusion.Closed)
+        }))
+    .Overlap(new[]
+        {
+            new Interval<int>(15, 20, IntervalInclusion.Closed),
+            new Interval<int>(30, 35, IntervalInclusion.Closed)
+        }
+        .SymmetricDifference(new Interval<int>(20, 35, IntervalInclusion.Opened)))
+    .Overlap(new Interval<int>(20, 25, IntervalInclusion.Opened))
+    .ToArray();
 
-var nextQuarter = new QuarterInterval(2022, 3).GetNext();
+var nextQuarter = new QuarterInterval(2022, 3).GetNext().Combine(new MonthInterval(2022, 1).GetPrev());
 
 Console.WriteLine();
